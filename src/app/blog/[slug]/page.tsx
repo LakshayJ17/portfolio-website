1667d33b0c894cff4eaeb83e353de8a093d288fb
lakshay17 <lakshayjain01lj@gmail.com>
2025-08-02 23:36:12 +0530

    Refactor blog page to dynamically fetch content based on slug; update MDX handling in utils

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,26 +1,33 @@
 import { Container } from "@/components/container";
 import { Metadata } from "next";
-import { compileMDX, MDXRemote } from "next-mdx-remote/rsc";
 import { getSingleBlog } from "@/utils/mdx";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "All blogs - Lakshay Jain",
     description: "All my general wisdom and thoughts",
 };
 
-export default async function SingleBlogPage() {
-    const singleBlog = await getSingleBlog("tailwind-basics");
+export default async function SingleBlogPage({params}: {
+    params: {
+        slug: string;
+    };
+}) {
+    const {slug} = await params
+    const blog = await getSingleBlog(slug);
 
-    const { content, frontmatter } = await compileMDX<{ title: string }>({
-        source: singleBlog,
-        options: { parseFrontmatter: true },
-    });
+    if (!blog){
+        redirect('/blog')
+    }
+    
+    const {content, frontmatter} = blog
+    console.log(frontmatter);
 
     return (
         <div className="flex min-h-screen items-start justify-start">
             <Container className="min-h-[200vh] px-8 md:pt-20 md:pb-10">
                 <div className="prose">
-                    <MDXRemote source={singleBlog} />
+                    {content}
                 </div>
             </Container>
         </div>
--- src/utils/mdx.ts
+++ src/utils/mdx.ts
@@ -1,10 +1,27 @@
 import { promises as fs } from 'fs';
+import { compileMDX } from 'next-mdx-remote/rsc';
 import path from 'path';
 
 export const getSingleBlog = async (slug: string) => {
-    const singleBlog = await fs.readFile(
-        path.join(process.cwd(), "src/data", "tailwindcss-basics.mdx"),
-        "utf-8"
-    )
-    return singleBlog
+    try {
+        const singleBlog = await fs.readFile(
+            path.join(process.cwd(), "src/data", `${slug}.mdx`),
+            "utf-8"
+        );
+
+        if (!singleBlog) {
+            return null;
+        }
+
+        const { content, frontmatter } = await compileMDX<{ title: string }>({
+            source: singleBlog,
+            options: { parseFrontmatter: true },
+        });
+
+        return { content, frontmatter };
+
+    } catch (error) {
+        console.error("Error reading or compiling blog:", error);
+        return null
+    }
 }
